7ccdca24c25e1fb0e0d13c7184bf9a39a7d31d81
Leandro Coutinho <lescoutinhovr@hotmail.com>
2020-04-05 11:18:49 -0300

    it solves the easiest case of D. just one point ... xD

--- 2020/qualification-round/d/Solution.java
+++ 2020/qualification-round/d/Solution.java
@@ -21,6 +21,7 @@ public class Solution {
 		String[] tb = sc.nextLine().split(" ");
 		int T = Integer.parseInt(tb[0]);
 		int B = Integer.parseInt(tb[1]);
+		// System.err.println(T + " " + B);
 
 		for (int t = 1; t <= T; ++t) {
 			solve(t, B);
@@ -33,50 +34,52 @@ public class Solution {
 
 	private static void solve(int t, int B) {
 
-		HashSet<List<Integer>> hashSet = new HashSet<List<Integer>>();
-		Integer[] tmp1 = new Integer[B];
+		List<int[]> list = new ArrayList<int[]>();
+		int[] tmp1 = new int[B];
 		Arrays.fill(tmp1, -1);
-		hashSet.add(Arrays.asList(tmp1));
+		list.add(tmp1);
 
 		for (int i = 1; i <= 150; i++) {
 			for (int b = 0; b < B; ++b) {
 				out.println(b+1);
 				int bit = Integer.parseInt(sc.nextLine());
-				for (List<Integer> l : hashSet) {
-					l.set(b, bit);
+				// System.err.println(bit);
+				int size = list.size();
+				for (int j = 0; j < size; j++) {
+					int[] l = list.get(j);
 
-					if (i % 10 == 1) {
-						List<Integer> tmp = Arrays.asList(invert((Integer[])l.toArray()));
-						tmp.set(b, bit);
-						hashSet.add(tmp);
-						tmp = Arrays.asList(reverse((Integer[])l.toArray()));
-						tmp.set(b, bit);
-						hashSet.add(tmp);
-						tmp = Arrays.asList(complementationAndReversal((Integer[])l.toArray()));
-						tmp.set(b, bit);
-						hashSet.add(tmp);
+					l[b] = bit;
+
+					if (i > 1 && i % 10 == 1) {
+						int[] tmp = invert(l);
+						tmp[b] = bit;
+						list.add(tmp);
+						tmp = reverse(l);
+						tmp[b] = bit;
+						list.add(tmp);
+						tmp = complementationAndReversal(l);
+						tmp[b] = bit;
+						list.add(tmp);
 					}
 				}
 			}
-			if (hashSet.size() == 1) {
+			if (list.size() == 1) {
 				boolean isComplete = true;
-				for (List<Integer> l : hashSet) {
-					for (int j = 0; j < B; j++) {
-						if (l.get(j) == -1) {
-							isComplete = false;
-							break;
-						}
+				int[] l = list.get(0);
+				for (int j = 0; j < B; j++) {
+					if (l[j] == -1) {
+						isComplete = false;
+						break;
 					}
 				}
 				if (isComplete) {
 					StringBuilder answer = new StringBuilder();
-					for (List<Integer> l : hashSet) {
-						for (int j = 0; j < B; j++) {
-							answer.append(l.get(j));
-						}
+					for (int j = 0; j < B; j++) {
+						answer.append(l[j]);
 					}
 					out.println(answer.toString());
 					String reply = sc.nextLine();
+					System.err.println("Reply: " + reply);
 					if ("Y".equals(reply)) return;
 					else System.exit(-1);
 				}
@@ -85,12 +88,12 @@ public class Solution {
 		
 	}
 
-	private static Integer[] complementationAndReversal(Integer[] array) {
+	private static int[] complementationAndReversal(int[] array) {
 		return invert(reverse(array));
 	}
 
-	private static Integer[] invert(Integer[] array) {
-		Integer[] inverted = new Integer[array.length];
+	private static int[] invert(int[] array) {
+		int[] inverted = new int[array.length];
 
 		for (int i = 0; i < inverted.length; i++) {
 			inverted[i] = array[i] == 0 ? 1 : 0;
@@ -99,8 +102,8 @@ public class Solution {
 		return inverted;
 	}
 
-	private static Integer[] reverse(Integer[] array) {
-		Integer[] reversed = new Integer[array.length];
+	private static int[] reverse(int[] array) {
+		int[] reversed = new int[array.length];
 
 		for (int i = 0, j = array.length - 1; i < array.length/2; i++, j--) {
 			reversed[i] = array[j];
@@ -112,16 +115,16 @@ public class Solution {
 
 
 	private static void tests() {
-		Integer[] test = {0, 1, 0, 0, 1, 1};
+		int[] test = {0, 1, 0, 0, 1, 1};
 		System.out.print("--------- ORIGINAL ----------- ");
-		for (Integer i : test) System.out.print(i + " ");
+		for (int i : test) System.out.print(i + " ");
 
 		System.out.print("\n--------- INVERTED ----------- ");
-		Integer[] inverted = invert(test);		
-		for (Integer i : inverted) System.out.print(i + " ");
+		int[] inverted = invert(test);		
+		for (int i : inverted) System.out.print(i + " ");
 
 		System.out.print("\n--------- REVERTED ----------- ");
-		Integer[] reversed = reverse(test);
-		for (Integer i : reversed) System.out.print(i + " ");
+		int[] reversed = reverse(test);
+		for (int i : reversed) System.out.print(i + " ");
 	}
 }
